9025a6d20c6da338e18d26d673946ce56281a9f5
pradeep <pbpradeeptechno@gmail.com>
2014-07-21 15:42:08 +0530

    user account options(edit,view) added

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1,61 +1,113 @@
 <?php
+App::uses('AppController', 'Controller');
+/**
+ * Users Controller
+ *
+ * @property User $User
+ * @property PaginatorComponent $Paginator
+ * @property SessionComponent $Session
+ */
 class UsersController extends AppController {
-	
+
+
 	public function beforeFilter() {
 		parent::beforeFilter();
 		$this->Auth->allow('add', 'logout');
 	}
+/**
+ * Components
+ *
+ * @var array
+ */
+	public $components = array('Paginator', 'Session');
+
+/**
+ * index method
+ *
+ * @return void
+ */
 	public function index() {
 		$this->User->recursive = 0;
-		$this->set('users', $this->paginate());
+		$this->set('users', $this->Paginator->paginate());
 	}
+
+/**
+ * view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
 	public function view($id = null) {
-		$this->User->id = $id;
-		if(!$this->User->exists()) {
+		if (!$this->User->exists($id)) {
 			throw new NotFoundException(__('Invalid user'));
 		}
-		$this->set('user', $this->User->read(null, $id));
+		$options = array('conditions' => array('User.' . $this->User->primaryKey => $id));
+		$this->set('user', $this->User->find('first', $options));
 	}
+
+/**
+ * add method
+ *
+ * @return void
+ */
 	public function add() {
-		if($this->request->is('post')) {
+		if ($this->request->is('post')) {
 			$this->User->create();
-			if($this->User->save($this->request->data)) {
-				$this->Session->setFlash(__('The user has been saved'));
-				return $this->rdirect(array('action' => 'index'));
+			if ($this->User->save($this->request->data)) {
+				$this->Session->setFlash(__('The user has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The user could not be saved. Please, try again.'));
 			}
-			$this->Session->setFlash(__('The user could not be saved. Please, try again.'));
 		}
 	}
+
+/**
+ * edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
 	public function edit($id = null) {
-		$this->User->id = $id;
-		if(!$this->user->exists()) {
+		if (!$this->User->exists($id)) {
 			throw new NotFoundException(__('Invalid user'));
 		}
-		if($this->request->is('post') || $this->request->is('put')) {
-			if($this->User->save($this->request->data)) {
-				$this->Session->setFlash(__('The user has been saved'));
+		if ($this->request->is(array('post', 'put'))) {
+			if ($this->User->save($this->request->data)) {
+				$this->Session->setFlash(__('The user has been saved.'));
 				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The user could not be saved. Please, try again.'));
 			}
-			$this->Session->setFlash(__('The user could not be saved. Please, try again.'));
-		}else {
-			$this->request->data = $this->User->read(null, $id);
-			unset($this->request->data['User']['password']);
+		} else {
+			$options = array('conditions' => array('User.' . $this->User->primaryKey => $id));
+			$this->request->data = $this->User->find('first', $options);
 		}
 	}
+
+/**
+ * delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
 	public function delete($id = null) {
-		$this->request->onlyAllow('post');
-		
 		$this->User->id = $id;
 		if (!$this->User->exists()) {
 			throw new NotFoundException(__('Invalid user'));
 		}
+		$this->request->allowMethod('post', 'delete');
 		if ($this->User->delete()) {
-			$this->Session->setFlash(__('User deleted'));
-			return $this->redirect(array('action' => 'index'));
+			$this->Session->setFlash(__('The user has been deleted.'));
+		} else {
+			$this->Session->setFlash(__('The user could not be deleted. Please, try again.'));
 		}
-		$this->Session->setFlash(__('User was not deleted'));
 		return $this->redirect(array('action' => 'index'));
 	}
+
 	public function login() {
 		if($this->request->is('post')) {
 			if($this->Auth->login()) {
@@ -68,4 +120,3 @@ class UsersController extends AppController {
 		return $this->redirect($this->Auth->logout());
 	}
 }
-?>
--- app/View/Users/index.ctp
+++ app/View/Users/index.ctp
@@ -1,7 +1,9 @@
 <div class="users index">
 	<h2><?php echo __('Users'); ?></h2>
 	<table cellpadding="0" cellspacing="0">
+	<thead>
 	<tr>
+			<th><?php echo $this->Paginator->sort('id'); ?></th>
 			<th><?php echo $this->Paginator->sort('username'); ?></th>
 			<th><?php echo $this->Paginator->sort('password'); ?></th>
 			<th><?php echo $this->Paginator->sort('role'); ?></th>
@@ -9,8 +11,11 @@
 			<th><?php echo $this->Paginator->sort('modified'); ?></th>
 			<th class="actions"><?php echo __('Actions'); ?></th>
 	</tr>
+	</thead>
+	<tbody>
 	<?php foreach ($users as $user): ?>
 	<tr>
+		<td><?php echo h($user['User']['id']); ?>&nbsp;</td>
 		<td><?php echo h($user['User']['username']); ?>&nbsp;</td>
 		<td><?php echo h($user['User']['password']); ?>&nbsp;</td>
 		<td><?php echo h($user['User']['role']); ?>&nbsp;</td>
@@ -19,10 +24,11 @@
 		<td class="actions">
 			<?php echo $this->Html->link(__('View'), array('action' => 'view', $user['User']['id'])); ?>
 			<?php echo $this->Html->link(__('Edit'), array('action' => 'edit', $user['User']['id'])); ?>
-			<?php echo $this->Form->postLink(__('Delete'), array('action' => 'delete', $user['User']['id']), null, __('Are you sure you want to delete # %s?', $user['User']['id'])); ?>
+			<?php echo $this->Form->postLink(__('Delete'), array('action' => 'delete', $user['User']['id']), array(), __('Are you sure you want to delete # %s?', $user['User']['id'])); ?>
 		</td>
 	</tr>
 <?php endforeach; ?>
+	</tbody>
 	</table>
 	<p>
 	<?php
