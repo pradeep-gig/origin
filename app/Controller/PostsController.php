ed69ea4e2957daf760dfe0c1274c0a3457a28d90
pradeep <pbpradeeptechno@gmail.com>
2014-07-21 11:57:43 +0530

    multiuser model done

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -18,7 +18,7 @@
 		}
 		public function add() {
 			if($this->request->is('post')) {
-				$this->Post->create();
+				$this->request->data['Post']['user_id'] = $this->Auth->user('id');
 				if($this->Post->save($this->request->data)) {
 					$this->Session->setFlash(__('Your post has been saved.'));
 					return $this->redirect(array('action' => 'index'));
@@ -54,6 +54,29 @@
 				$this->Session->setFlash(__('The post with id: %s has been deleted.', h($id)));
 				return $this->redirect(array('action' => 'index'));
 			}
+		}
+		public function isAuthorized($user) {
+			// All registered users can add posts
+			if ($this->action === 'add') {
+				return true;
+			}
+			// The owner of a post can edit and delete it
+			if (in_array($this->action, array('edit', 'delete'))) {
+				$postId = (int) $this->request->params['pass'][0];
+				if ($this->Post->isOwnedBy($postId, $user['id'])) {
+					return true;
+				}
+			}
+			return parent::isAuthorized($user);
+				if($this->action === add) {
+					return true;
+				}
+			if(in_array($this->action, array('edit', 'delete'))) {
+				$postId = (int) $this->request->params['pass'][0];
+				if($this->Post->isownedBy($postId, $user['id']));{
+					return true;				
+				}
+			}	
 		}
 	}
 ?> 
